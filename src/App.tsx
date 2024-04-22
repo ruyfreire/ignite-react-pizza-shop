f03a3b1b1ea7392648c228840560370b3308f803
Ruy Freire <ruy.freire@provu.com.br>
2024-04-22 18:28:09 -0300

    visual completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { ThemeProvider } from '@/components/theme/theme-provider'
+
+import { router } from './routes'
+
 export function App() {
   return (
-    <h1>Hello World</h1>
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizza.shop">
+        <Helmet titleTemplate="%s | Pizza Shop" />
+        <Toaster richColors />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
   )
 }
